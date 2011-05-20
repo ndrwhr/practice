f8ea2bd21649d5fdc6a03e81349b498c39fe095e
Andrew Hoyer <me@andrew-hoyer.com>
2011-05-20 01:11:02 -0700

    added a swap method

--- bubblesort.js
+++ bubblesort.js
@@ -5,13 +5,9 @@ Array.implement('bubblesort', function(){
 
     do {
         swap = null;
-        for (i = 1; i < length; i++){
-            if (this[i - 1] > this[i]){
-                swap = this[i - 1];
-                this[i - 1] = this[i];
-                this[i] = swap;
-            }
-        }
+        for (i = 1; i < length; i++)
+            if (this[i - 1] > this[i])
+                swap = this.swap(i, i - 1);
     } while (swap != null);
 
     return this;
--- insertionsort.js
+++ insertionsort.js
@@ -3,15 +3,10 @@ Array.implement('insertionsort', function(){
     var length = this.length,
         swap, i, j;
 
-    for (i = 1; i < length; i++){
-        if (this[i - 1] > this[i]){
-            for (j = i; j > 0 && this[j - 1] > this[j]; j--){
-                swap = this[j - 1];
-                this[j - 1] = this[j];
-                this[j] = swap;
-            }
-        }
-    }
+    for (i = 1; i < length; i++)
+        if (this[i - 1] > this[i])
+            for (j = i; j > 0 && this[j - 1] > this[j]; j--)
+                this.swap(j, j - 1);
 
     return this;
 });

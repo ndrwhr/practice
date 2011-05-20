e3e1928a648cd84094388b2efbc430a55acacfb4
Andrew Hoyer <me@andrew-hoyer.com>
2011-05-20 01:11:12 -0700

    added selection sort

---
+++ selectionsort.js
@@ -0,0 +1,15 @@
+
+Array.implement('selectionsort', function(){
+    var length = this.length,
+        swap, i, j;
+
+    for (i = 0; i < length - 1; i++){
+        for (min = j = i; j < length; j++)
+            if (Math.min(this[min], this[j]) == this[j])
+                min = j;
+
+        this.swap(i, min);
+    }
+
+    return this;
+});
